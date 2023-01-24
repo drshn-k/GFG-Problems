4d5169f7671b853130f61f64af6f67f208b7a778
drshn-k <110783201+drshn-k@users.noreply.github.com>
2023-01-24 11:43:11 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Convert an array to reduced form/convert-an-array-to-reduced-form.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Converts arr[0..n-1] to reduced form.
+	void convert(int arr[], int n) {
+	    // code here
+	    set<int> s(arr, arr+n);
+	    unordered_map<int , int> mp;
+	    
+	   // for(auto i : s) cout << i << ' ';
+	   // cout << endl;
+	    int num = 0;
+	    for(auto i : s){
+	        mp[i] = num;
+	        num = num + 1;
+	    }
+	   // for(auto i :mp) cout << i.first << " " << i.second << endl;
+	    for(int i = 0 ; i < n ; i++){
+	        arr[i] = mp[arr[i]];
+	    }
+	    
+	   
+	    
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.convert(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
