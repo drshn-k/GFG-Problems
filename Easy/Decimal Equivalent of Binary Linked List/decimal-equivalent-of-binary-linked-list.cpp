d055f5f75f97a2d14f0ae2d1a0df4b1d1d7d4abe
Darshan Kasar <110783201+drshn-k@users.noreply.github.com>
2024-02-03 11:13:47 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Decimal Equivalent of Binary Linked List/decimal-equivalent-of-binary-linked-list.cpp
@@ -0,0 +1,119 @@
+//{ Driver Code Starts
+// Program to check if linked list is empty or not
+#include<iostream>
+using namespace std;
+const long long unsigned int MOD = 1000000007;
+
+/* Link list Node */
+struct Node
+{
+    bool data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+
+// } Driver Code Ends
+/* Below global variable is declared in code for modulo arithmetic
+const long long unsigned int MOD = 1000000007; */
+
+/* Link list Node/
+struct Node
+{
+    bool data;   // NOTE data is bool
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+
+class Solution
+{   long long unsigned int mod = 1e9+7;
+    
+    long long unsigned int modPow(long long unsigned int base, long long unsigned int exp) {
+        long long unsigned int result = 1;
+        while (exp > 0) {
+            if (exp % 2 == 1) {
+                result = (result * base) % mod;
+            }
+            base = (base * base) % mod;
+            exp /= 2;
+        }
+        return result;
+    }
+    
+public:
+    long long unsigned int decimalValue(Node *head) {
+        long long unsigned int res = 0;
+        Node* ptr = head;
+        int length = 0;
+
+        while (ptr != nullptr) {
+            length++;
+            ptr = ptr->next;
+        }
+        
+        ptr = head;
+        while (ptr != nullptr) {
+            length--;
+            if (ptr->data) {
+                res = (res + modPow(2, length)) % mod;
+            }
+            ptr = ptr->next;
+        }
+        
+        return res;
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+void append(struct Node** head_ref, struct Node **tail_ref, bool new_data)
+{
+
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+
+bool isEmpty(struct Node *head);
+
+/* Driver program to test above function*/
+int main()
+{
+    bool l;
+    int i, n, T;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+        Solution obj;
+        cout << obj.decimalValue(head) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
