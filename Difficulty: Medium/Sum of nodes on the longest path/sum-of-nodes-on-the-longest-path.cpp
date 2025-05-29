5c0edc69e1318a75213b420c9df49c63c9d5412c
Darshan Kasar <110783201+drshn-k@users.noreply.github.com>
2025-05-30 00:25:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of nodes on the longest path/sum-of-nodes-on-the-longest-path.cpp
@@ -0,0 +1,40 @@
+/*
+class Node {
+  public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int x) {
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+     void h(Node* root, int cur, int len, int &max_, int &sum) {
+        if(root==NULL)
+            return;
+        cur+=root->data;
+        len++;
+        if(root->left==NULL&&root->right==NULL) {
+            if(len>max_) {
+                max_=len;
+                sum =cur;
+            } else if(len == max_) 
+                sum=max(sum, cur);
+            return;
+        }
+        h(root->left,cur,len, max_,sum);
+        h(root->right,cur, len,max_,sum);
+    }
+
+    int sumOfLongRootToLeafPath(Node *root) {
+        int max_ =0;
+        int sum=0;
+        h(root,0,0, max_,sum);
+        return sum;
+    }
+};
